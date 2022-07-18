60fb38d9e54aaf1f3988903d979b6b96872b5519
ibrahimkaddour <ibrahimkaddour@gmail.com>
2022-07-18 12:02:56 +0300

    edit

--- ahcec_hr_loan/models/hr_payroll.py
+++ ahcec_hr_loan/models/hr_payroll.py
@@ -80,7 +80,7 @@ class HrPayslip(models.Model):
                             'amount_python_compute': rule.amount_python_compute,
                             'amount_percentage': rule.amount_percentage,
                             'amount_percentage_base': rule.amount_percentage_base,
-                            'register_id': rule.register_id.id,
+                            
                             'amount': -(loan.deduction_amount),
                             'employee_id': payslip.employee_id.id,
                         }
